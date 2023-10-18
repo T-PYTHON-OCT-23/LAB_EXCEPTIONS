a61ab5295331e4bdbc3fa2ee5862b666375a05a3
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-10-18 11:55:32 +0300

    the lab is solved

---
+++ lab_exception.py
@@ -0,0 +1,15 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+try:
+    print("Addition:", x + b)
+except ValueError as e:
+    print(e)
+else:
+    print("the operation is successful")
+finally:
+    print("The error is the variable (b) is not defined")
+
+
+    additoin(10, 20)
+
