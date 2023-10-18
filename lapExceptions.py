1497930777f5aa6ffaed9834c10bb40fc56fe5b9
alya <alyamem80@gmail.com>
2023-10-18 13:11:38 +0300

    lapexceptionSolve

---
+++ lapExceptions.py
@@ -0,0 +1,16 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+ 
+try:
+    additoin(10 , 20)# call function inside try......
+    print("Addition:", x + b)#b is not defined
+   # additoin(10, 20)
+except Exception  as e:
+ print("'b' is not defined")
+
+
+
+
+
+
