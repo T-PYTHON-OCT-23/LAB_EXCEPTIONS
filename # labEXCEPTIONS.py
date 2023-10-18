3868c820086bf71e3e70b5d1c5c1a471e28dbf2b
Reef <reef.zayed2@gmail.com>
2023-10-18 12:13:08 +0300

    Done_labExcep

---
+++ # labEXCEPTIONS.py
@@ -0,0 +1,14 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+try:
+    additoin(10, 20)
+    print("the operation is successful")
+except NameError:
+    print("This error name 'b' is not defined")
+
+
+    
+
