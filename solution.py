cf71a30e41623159b69ca502a954eeae52fd943d
lama1-abdullah <lama.alothemen21@gmail.com>
2023-10-26 22:24:56 +0300

    done .

--- solution.py
+++ solution.py
@@ -1,9 +1,27 @@
 def additoin(x, y):
     x = 10
     y = 20
-    print("Addition:", x + b)
-
- try:
+try:
+     
+    result = x + b  
+    print("The operation is successful")
     additoin(10, 20)
-except NameError:
-    print("There is an error in the name")
+
+
+except NameError as e:
+     print("an error occurred:", e)
+except Exception as e:
+     print(e)
+
+
+
+
+
+
+
+
+
+
+     
+   
+     
